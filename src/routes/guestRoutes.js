72c8fe3aabe5941013cb9c4431ea0abe29ebe748
akinwale habib <akinwalehabib@hotmail.com>
2019-01-26 16:06:22 +0100

    ft #163498724 added get guest list

--- src/routes/guestRoutes.js
+++ src/routes/guestRoutes.js
@@ -7,13 +7,14 @@ const guestRouter = express.Router();
 
 guestRouter.route('/')
     .post((req, res) => {
+        let pool;
         const { email, name, gender } = req.body;
         const text = 'INSERT INTO guestList(email, name, gender) VALUES($1, $2, $3) RETURNING *'
         const values = [email, name, gender];
 
         try {
             // create new pool
-            const pool = new Pool();
+            pool = new Pool();
         } catch (error) {
             res.status(500).json(error);
         }
@@ -28,6 +29,44 @@ guestRouter.route('/')
                 }
                 await pool.end();
                 res.json(data);
+            } catch (error) {
+                res.status(500).json(error);
+            }
+        }());
+    })
+
+guestRouter.route('/guestList')
+    .get((req, res) => {
+        const text = 'SELECT email, name, gender FROM guestList INNER JOIN rsvp ON (guestList.email=rsvp.guest)'
+        let pool;
+
+        try {
+            // create new pool
+            pool = new Pool();
+        } catch (error) {
+            res.status(500).json(error);
+        }
+        
+
+        (async function queryDB(){
+            try {
+                const response = await pool.query(text);
+                if (response.rowCount) {
+                    const data = {
+                        status: 200,
+                        message: 'Here are the guests we are expecting',
+                        guests: response.rows[0]
+                    }
+                    await pool.end();
+                    res.json(data);
+                } else {
+                    const data = {
+                        status: 200,
+                        message: 'We expect no guests today'
+                    }
+                    await pool.end()
+                    res.json({data})
+                }
             } catch (error) {
                 res.status(500).json(error);
             }
